0602acce4b863c83be3da97ed831afaf7736f845
stalkersx <ryomahendra1520@gmail.com>
2024-01-28 10:12:26 +0700

    # version 1.0

--- example_gtk/b_python/window.py
+++ example_gtk/b_python/window.py
@@ -11,8 +11,10 @@ class window(Gtk.Window):
 				Gtk.Window.__init__(self, title="judul")
 				Gtk.Window.set_default_size(self, 300, 150)
 				Gtk.Window.set_position(self, Gtk.WindowPosition.CENTER)
+				#Gtk.Window.set_position(self, Gtk.WindowPosition.MOUSE)
 				
 win = window()
+#win.set_icon_from_file(loci + image)
 win.connect("delete-event", Gtk.main_quit)
 win.show_all()
 Gtk.main()
